9fe092836dc560a50ab8baf9403ec13671a40dca
Lanting Chiang <lantingchiang@gmail.com>
2020-07-17 08:57:56 -0400

    Added styling

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -8,16 +8,16 @@ export default class Exercises extends Component {
           {this.props.exercises.map(exercise => (
             <li key={exercise._id}>
               <div className="exercise">
+                <img src={exercise.image} alt={exercise.title}></img>
                 <a href={"#" + exercise._id}>
-                  <img src={exercise.image} alt={exercise.title}></img>
-                  <p>{exercise.title}</p>
+                  <div className="title">{exercise.title}</div>
                 </a>
-                <div className="exercise-type">
-                  <div>Description: {exercise.description}</div>
-                  <div>Type: {exercise.type}</div>
-                  <div>Equipment: {exercise.equipment} </div>
-                  <button className="button-primary">Join!</button>
-                </div>
+              </div>
+              <div className="exercise-details">
+                <div>Description: {exercise.description}</div>
+                <div>Type: {exercise.type}</div>
+                <div>Equipment: {exercise.equipment} </div>
+                <button className="button-primary">Join!</button>
               </div>
             </li>
           ))}
