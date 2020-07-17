233c01c93d834c00cbf0c5a8e5f0b15aa33c766d
Lanting Chiang <lantingchiang@gmail.com>
2020-07-17 09:47:14 -0400

    CREATE FILTER COMPONENT
    Filters based on type of exercise and sort exercise by time of creation or popularity.
    Add field "count" denoting the number of people who joined the exercise to the exercise
    component in order to sort by popularity. Sort by time of creation was based on the
    assumption that exercises with a later creation time will have a larger id. May need to
    refactor after connecting to db. Need to add feature where count increments as users
    join the challenge.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from "./data.json";
 import Exercises from './components/Exercises';
+import Filter from './components/Filter';
 
 class App extends React.Component {
   constructor() {
@@ -8,9 +9,41 @@ class App extends React.Component {
     this.state = {
       exercises: data.exercises,
       type: "",
-      equipment: "",
+      sort: "",
     }
   }
+
+  sortExercises = (event) => {
+    const sortBy = event.target.value;
+    console.log(sortBy);
+    this.setState((state) => ({
+      sort: sortBy,
+      exercises: this.state.exercises.slice()
+        .sort((a, b) =>
+          sortBy === "popularity"
+            ? a.count > b.count
+              ? -1
+              : 1
+            : a._id > b._id
+              ? -1
+              : 1
+        ),
+    }))
+  }
+
+  filterExercises = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ type: event.target.value, exercises: data.exercises })
+    } else {
+      this.setState({
+        type: event.target.value,
+        exercises: data.exercises.filter(exercise =>
+          exercise.type.toLowerCase().indexOf(event.target.value) >= 0)
+      })
+    }
+  }
+
   render() {
     return (
       <div className="grid-container">
@@ -20,6 +53,13 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.exercises.length}
+                type={this.state.type}
+                sort={this.state.sort}
+                filterExercises={this.filterExercises}
+                sortExercises={this.sortExercises}
+              ></Filter>
               <Exercises exercises={this.state.exercises}></Exercises>
             </div>
             <div className="sidebar">Exercises joined</div>
